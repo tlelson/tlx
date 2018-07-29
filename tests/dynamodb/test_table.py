c4826b7caebffc35b339c5111e90d1acc71196df
eL0ck <tpj800@gmail.com>
2018-07-29 10:32:17 +1000

    comment and clean

---
+++ tests/dynamodb/test_table.py
@@ -0,0 +1,16 @@
+import os
+import json
+from decimal import Decimal
+from unittest import TestCase
+import tlx.dynamodb.table
+
+
+class TestTable(TestCase):
+    def test_append_to_list_field(self):
+        msg = 'New item should be appended'
+        pass
+
+    def test_add_new_map_field(self):
+        msg = 'Field should be added'
+        pass
+
--- tlx/dynamodb/table.py
+++ tlx/dynamodb/table.py
@@ -10,22 +10,17 @@ def add_key(table, key, item):
     """
 
     key_names = [k for k in key]
-    item_template = {**key, **item}
-    logger.debug(f'submitting item: {item_template}')
+    full_item = {**key, **item}
+    logger.debug(f'submitting item: {full_item}')
 
-    try:
-        logger.info(f"Attempting to add new record for: {key} ")
-        res = table.put_item(
-            Item=item_template,
-            ConditionExpression=f"attribute_not_exists({key_names[0]})",  # TODO fix for items with Partition and Sort key
-        )
-    except Exception as e:
-        msg = f'Failed to add {item_template}: {e}'
-        logger.error(msg)
-        raise APIGException(msg, code=500)
+    logger.info(f"Attempting to add new record for: {key} ")
+    res = table.put_item(
+        Item=full_item,
+        ConditionExpression=f"attribute_not_exists({key_names[0]})",  # TODO fix for items with Partition and Sort key
+    )
 
     logger.info(f"Successfully added new record.")
-    logger.debug(f"{item_template}")
+    logger.debug(f"{full_item}")
     return res['ResponseMetadata']['HTTPStatusCode']
 
 
@@ -38,10 +33,11 @@ def append_to_list_field(table, key, field_to_update, expression_attribute_names
         Args:
             table (boto3 Table):
             key (dict): Primary key for the table. e.g {'matchid': matchid}
-            field_to_update (list): ['providers', '#dataproviderid'].  Variables for substitution should start with a hash and
-                have values defined in `expression_attribute_names`.
-            expression_attribute_names (dict): Expansion of variables names in the field to update. E.g {'#dataproviderid': "OPTA"}
-            new_item (list): To be appended to a list the object must be a list.
+            field_to_update (list): e.g ['providers', '#dataproviderid'].  Variables for substitution should
+                                    start with a hash and have values defined in `expression_attribute_names`.
+            expression_attribute_names (dict): Expansion of variables names in the field to update.
+                                               E.g {'#dataproviderid': "BILL"}
+            new_item (list): To be appended to a list, the object must be a list.
             add_missing_key (bool): Default False. If True add the primary key for the table if it is not found.
                                     Otherwise returns dict of data to be added by user.
 
@@ -52,6 +48,8 @@ def append_to_list_field(table, key, field_to_update, expression_attribute_names
     try:
         str_path = '.'.join(field_to_update)
         # TODO: Consider adding a check for if key exists here rather than in the `_add_new_map_field` call stack
+        # Current model is optimised for frequent updates of existing keys.  If keys are frequently being added
+        # it would be more efficient to try adding the key first and then this method.
         res = table.update_item(
             Key=key,
             UpdateExpression=f"SET {str_path} = list_append(:new_item, {str_path})",
@@ -72,8 +70,8 @@ def append_to_list_field(table, key, field_to_update, expression_attribute_names
 def add_new_map_field(table, key, path, field_to_add, expression_attribute_names, data, add_missing_key=False):
     """ Adds a new field (field_to_add) to an existing path in a map (path) for a given Primary key (key).
 
-        N.B If the path doesn't exist attempts to create that path.  If the key doesn't exist, will attempt to
-        create IFF add_missing_key=True.
+        N.B If the path doesn't exist, the function attempts to create that path.  If the key doesn't exist,
+        it will attempt to create IFF `add_missing_key=True`.
 
         Args:
             table (boto3 Table):
@@ -82,8 +80,8 @@ def add_new_map_field(table, key, path, field_to_add, expression_attribute_names
                          Specify that value in `expression_attribute_names`. E.g ['root', '#firstkey', '2ndkey']
             field_to_add (string): The name of the new field to add at the end of existing map described by `path`.
             expression_attribute_names (dict): Substitutes for the #values in `path`. E.g {'#firstkey': 'TeamMembers'}
-            data (any): Value to be saved under `path`+`field_to_add`.  Must be valid datatypes for Dynamodb.
-            add_missing_key (bool): Default False. If True add the primary key for the table if it is not found.
+            data (any): Value to be saved under `path`+`field_to_add`.  Must be valid datatype for Dynamodb.
+            add_missing_key (bool): Default False. If True, add the primary key for the table if it is not found.
                                     Otherwise returns dict of data to be added by user.
          Returns:
             int (200)                   - If successfully added item to Dynamodb
@@ -114,12 +112,3 @@ def add_new_map_field(table, key, path, field_to_add, expression_attribute_names
 
     # Recursively try to add until we empty the 'path' variable
     return add_new_map_field(table, key, path[:-1], path[-1], expression_attribute_names, new_data)
-
-
-def get_item(table, key):
-    """Get latest item for a given key otherwise returns an empty array"""
-
-    return table.get_item(
-        Key=key,
-        ConsistentRead=True,
-    ).get('Item', [])
