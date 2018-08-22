bbc36e63684fe44586c4184ad98ea25a00d40d78
eL0ck <tpj800@gmail.com>
2018-08-22 14:15:27 +1000

    Removing unnessesary cli apps

--- tests/basic_cli_tests.sh
+++ tests/basic_cli_tests.sh
@@ -4,6 +4,4 @@ set -ev
 # All command line apps should at least print their help message on all
 # platforms
 get-aws-creds --help
-dbp --help
-dynamo-batch-prepare --help
 dynamo-batch-write --help
