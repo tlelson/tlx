a1c32a219806e25497a80c9357dbb4232d48264a
eL0ck <tpj800@gmail.com>
2019-08-19 17:10:15 +1000

    remove auto indent.  Can be passed as kwarg if needed

--- tlx/dynamodb/aux.py
+++ tlx/dynamodb/aux.py
@@ -17,8 +17,8 @@ def json_loads(x, **kwargs):
 
 
 def json_dumps(x, **kwargs):
-    """Dumps json for DynamoDB (Numerical types go to Decimal)"""
-    return json.dumps(x, cls=DecimalEncoder, indent=4, separators=(',', ': '), sort_keys=True, **kwargs)
+    """Dumps json for DynamoDB (Numerical types go to Decimal) use indent=4 if required"""
+    return json.dumps(x, cls=DecimalEncoder, separators=(',', ': '), sort_keys=True, **kwargs)
 
 
 def string_from_datetime(d, sep=None):
