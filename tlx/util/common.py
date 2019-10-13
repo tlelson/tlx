8e65e80733984681de86e162d58fe9a0a9ac8d18
eL0ck <tpj800@gmail.com>
2019-10-14 10:36:37 +1100

    add more descriptive comments to support pandas datetime problem without importing it

--- tlx/util/common.py
+++ tlx/util/common.py
@@ -13,9 +13,19 @@ def get_uuid():
     return uuid4().hex
 
 
-def string_from_datetime(d, sep=None):
+def string_from_datetime(dt_obj, sep=None, timespec='milliseconds'):
     """string format from python datetime"""
-    return d.replace(tzinfo=None).isoformat(sep=sep, timespec='milliseconds')
+    # Pandas datetime is not yet implimenting `timespec`
+    # https://github.com/pandas-dev/pandas/issues/26131
+    try:
+        return dt_obj.replace(tzinfo=None).isoformat(sep=sep, timespec=timespec)
+    except TypeError as e:
+        msg = ("\n\nATTN!! If your datetime is actually "
+               "`pandas._libs.tslibs.timestamps.Timestamp`"
+               " then you may raise this exception.  Use `to_pydatetime` "
+               "to convert it.\n\n")
+        print(msg)
+        raise e
 
 
 def get_random_eventid():
