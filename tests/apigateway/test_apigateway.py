8b971bf915ce31269ea5c4d0e1893484f00f24b8
tle <tpj800@gmail.com>
2021-02-26 12:43:29 +1000

    APIGateway: Allow Use on AppSync Endpoints
    When configured as an appsync resolver an aws lambda may receive an
    event of any form.
    
    The 'message' field is removed since the statusCode indicates success or
    failure and the user may format their response to include whatever they
    like knowing that it will be json.dump'ed.
    
    no issue #

--- tests/apigateway/test_apigateway.py
+++ tests/apigateway/test_apigateway.py
@@ -1,114 +1,111 @@
-from tlx.apigateway import proxy_response_handler, APIGException, require_valid_inputs
-from tlx.dynamodb.aux import json_loads
 from unittest import TestCase
+
+from tlx.apigateway import (APIGException, proxy_response_handler,
+                            require_valid_inputs)
+from tlx.dynamodb.aux import json_loads
 
 
 class TestRequireFieldsFound(TestCase):
-
     def test_raises(self):
-        msg = 'An exception should be raised if the required fields are not present'
+        msg = "An exception should be raised if the required fields are not present"
 
-        supplied = {'first': 1, 'nineteenth': 2}
-        required = {'first', 'second'}
+        supplied = {"first": 1, "nineteenth": 2}
+        required = {"first", "second"}
 
         with self.assertRaises(Exception, msg=msg):
             require_valid_inputs(supplied, required)
 
     def test_passes(self):
-        msg = 'Nothing should be returned if the required fields are present'
+        msg = "Nothing should be returned if the required fields are present"
 
-        supplied = {'first': 1, 'nineteenth': 2}
-        required = {'first'}
+        supplied = {"first": 1, "nineteenth": 2}
+        required = {"first"}
 
         self.assertIsNone(require_valid_inputs(supplied, required), msg)
 
     def test_bad_params1(self):
-        msg = 'An exception should be raised if a parameter key is not a hashable type (str, int, float)'
+        msg = "An exception should be raised if a parameter key is not a hashable type (str, int, float)"
 
-        supplied = [{'first': 1, 'nineteenth': 2}]
-        required = {'first'}
+        supplied = [{"first": 1, "nineteenth": 2}]
+        required = {"first"}
 
         with self.assertRaises(Exception, msg=msg):
             require_valid_inputs(supplied, required)
 
     def test_bad_params2(self):
-        msg = 'An exception should be raised if a parameter key is not a hashable type (str, int, float)'
+        msg = "An exception should be raised if a parameter key is not a hashable type (str, int, float)"
 
         supplied = [True, None]
-        required = {'first'}
+        required = {"first"}
 
         with self.assertRaises(Exception, msg=msg):
             require_valid_inputs(supplied, required)
 
 
 class TestProxyResponseHandler(TestCase):
-    apig_event = {'resource': 'yoyo', 'queryStringParameters': None, 'body': None}
+    apig_event = {"resource": "yoyo", "queryStringParameters": None, "body": None}
 
     def test_malformed_event(self):
-        msg = "An event without the schema of an API Gateway event should raise and exception"
-
         @proxy_response_handler
         def dummy_handler(event, context):
-            print("received: {(event, context)}".formal(**locals()))
+            print("received: {(event, context)}".format(**locals()))
 
-        with self.assertRaises(Exception, msg=msg):
-            dummy_handler({}, {})
+        dummy_handler({}, {})
 
     def test_raised_exception_quiet(self):
-        msg = 'A generic exception should return a PROXY RESPONSE with status 500'
+        msg = "A generic exception should return a PROXY RESPONSE with status 500"
 
         @proxy_response_handler(quiet=True)
         def dummy_handler(event, context):
             raise Exception
 
         res = dummy_handler(self.apig_event, {})
-        self.assertEqual(res['statusCode'], 500, msg)
-        body = json_loads(res['body'])
-        self.assertEqual(body["message"], 'Error', msg)
+        self.assertEqual(res["statusCode"], 500, msg)
+        body = json_loads(res["body"])
+        self.assertEqual(body, {}, msg)
 
     def test_raised_exception_verbose(self):
-        msg = 'A exception should return a PROXY RESPONSE with status 500 AND report the exception message'
+        msg = "A exception should return a PROXY RESPONSE with status 500 AND report the exception message"
 
         @proxy_response_handler(quiet=False)
         def dummy_handler(event, context):
-            raise Exception('test')
+            raise Exception("test")
 
         res = dummy_handler(self.apig_event, {})
-        self.assertEqual(res['statusCode'], 500, msg)
-        body = json_loads(res['body'])
-        self.assertEqual(body["message"], 'Error: test', msg)
+        self.assertEqual(res["statusCode"], 500, msg)
+        body = json_loads(res["body"])
+        self.assertEqual(body, "Error: test", msg)
 
     def test_raised_exception_running_local(self):
-        msg = 'When running_local=True the exception should bubble up'
+        msg = "When running_local=True the exception should bubble up"
 
         @proxy_response_handler(running_local=True, quiet=False)
         def dummy_handler(event, context):
-            raise Exception('test')
+            raise Exception("test")
 
         with self.assertRaises(Exception, msg=msg):
             dummy_handler(self.apig_event, {})
 
     def test_raised_APIGException(self):
-        msg = 'Raising an APIGException should return a PROXY RESPONSE with the desired status'
+        msg = "Raising an APIGException should return a PROXY RESPONSE with the desired status"
 
         @proxy_response_handler
         def dummy_handler(event, context):
-            raise APIGException('yoyo', code=404)
+            raise APIGException("yoyo", code=404)
 
         res = dummy_handler(self.apig_event, {})
-        self.assertEqual(res['statusCode'], 404, msg)
-        body = json_loads(res['body'])
-        self.assertEqual(body["message"], 'Error: yoyo', msg)
+        self.assertEqual(res["statusCode"], 404, msg)
+        body = json_loads(res["body"])
+        self.assertEqual(body, "Error: yoyo", msg)
 
     def test_200(self):
-        msg = 'An exception free execution of the handler should return 200 with the results'
+        msg = "An exception free execution of the handler should return 200 with the results"
 
         @proxy_response_handler
         def dummy_handler(event, context):
             return "Hallo"
 
         res = dummy_handler(self.apig_event, {})
-        self.assertEqual(res['statusCode'], 200, msg)
-        body = json_loads(res['body'])
-        self.assertEqual(body["message"], 'Success', msg)
-        self.assertEqual(body["response"], 'Hallo', msg)
+        self.assertEqual(res["statusCode"], 200, msg)
+        body = json_loads(res["body"])
+        self.assertEqual(body, "Hallo", msg)
