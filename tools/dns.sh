2e321648a64d1bc6467c32ae510d65204cfa919c
Tim <tpj800@gmail.com>
2024-05-05 12:47:22 +1000

    Move Route53 Commands to dns.sh

---
+++ tools/dns.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+alias hosted-zones="aws route53 list-hosted-zones | jq -r '.HostedZones[].Name' "
+
+# TODO: tabulate
+record-sets() {
+	if [ "$#" -ne 0 ]; then
+		hz="$1"
+		cmd="aws route53 list-hosted-zones | jq --arg hz \"$hz\" '.HostedZones[] \
+				| select(.Name == \"$hz\") | .Id' "
+	else
+		cmd="aws route53 list-hosted-zones | jq '.HostedZones[] | .Id' "
+	fi
+
+	#echo "$cmd"
+	eval "$cmd" | xargs -I {} aws route53 \
+		list-resource-record-sets --hosted-zone-id '{}' | jq '
+		[.ResourceRecordSets[] | {Name, Type, Target:
+		(.AliasTarget.DNSName? // .ResourceRecords[].Value) }] | group_by(.Name) |
+		map({ Name: .[0].Name, Records: map("\(.Type) \(.Target)") })'
+
+	#jq '[.ResourceRecordSets[] | {Name, Type, Target: (.AliasTarget.DNSName? // .ResourceRecords[].Value) }] | group_by(.Name)'
+	#(aws) tools ❯ cat /tmp/record-sets.json | jq -r --slurp 'flatten | .[] | [.Name, .Type, .Target] |
+	#@tsv' | grep -v 'NS\|SOA'
+
+}
--- tools/misc.sh
+++ tools/misc.sh
@@ -1,4 +1,3 @@
-alias hosted-zones="aws route53 list-hosted-zones | jq -r '.HostedZones[].Name' "
 alias vpc-endpoint-connections="aws ec2 describe-vpc-endpoint-connections | jq -c '
   .VpcEndpointConnections[] | { ServiceId, VpcEndpointId, VpcEndpointState, Tags}'
 "
