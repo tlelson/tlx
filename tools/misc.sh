08a86c643b452fb96b0154ee486da0e8d3596909
Tim <tpj800@gmail.com>
2024-05-14 12:49:03 +1000

    Tools: Add security Group tools

--- tools/misc.sh
+++ tools/misc.sh
@@ -2,6 +2,36 @@
 
 alias aws-who-am-i='aws-list-accounts | grep "$(aws --output json sts get-caller-identity | jq -r "'".Account"'")"'
 
+alias security-groups="aws --output json ec2 describe-security-groups | jq -c '.SecurityGroups[] | {GroupId, GroupName, Description}'"
+
+security-group-rules() {
+	local help_text="Usage: ${FUNCNAME[0]} [OPTIONAL_ARGS] [options]
+	If security group is provided, all rules are returned.
+
+	Optional Arguments:
+	security-group-id	e.g sg-XXXX
+
+	Options:
+	--help       Display this help message"
+
+	# Check if the '--help' flag is present
+	if [[ "$*" == *"--help"* ]]; then
+		echo "$help_text"
+		return 0 # Exit the function after printing help
+	fi
+
+	cmd="aws ec2 describe-security-group-rules"
+	# Add filters if $1 is empty (meaning no argument provided)
+	if [ -n "$1" ]; then
+		cmd+=" --filters \"Name=group-id,Values=$1\""
+	fi
+
+	# Execute the constructed command string
+	eval "$cmd"
+
+}
+export -f security-group-rules
+
 alias load-balancers="aws --output json elbv2 describe-load-balancers | jq -r '.LoadBalancers[].LoadBalancerName' "
 
 load-balancer() {
