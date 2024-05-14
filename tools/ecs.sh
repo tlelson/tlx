4926db2070e15137ae9180341b7f2df9d2769f1e
Tim <tpj800@gmail.com>
2024-05-14 12:49:24 +1000

    Tools: Add ECS Task Def and improve Services

--- tools/ecs.sh
+++ tools/ecs.sh
@@ -2,15 +2,57 @@
 
 alias ecs-clusters="aws --output json ecs list-clusters | jq -r '.clusterArns[]'"
 
-ecs-services() {
-	# returns cluster/service (not full ARN)
-	{
-		echo "CLUSTER SERVICE"
-		ecs-clusters | xargs -P4 -n1 -I {} aws --output json ecs list-services --cluster {} |
-			jq -r '.serviceArns[] | sub("^[^/]+/"; "") | sub("/"; " ")'
-	} | column -t
+task-def() {
+	local help_text="Usage: ${FUNCNAME[0]} [OPTIONAL_ARGS] [options]
+	If no task definition is provided a list of active task definitons is returned
+
+	Optional Arguments:
+	task-definition	family-name:revision
+
+	Options:
+	--help       Display this help message"
+
+	# Check if the '--help' flag is present
+	if [[ "$*" == *"--help"* ]]; then
+		echo "$help_text"
+		return 0 # Exit the function after printing help
+	fi
+	if [ -n "$1" ]; then
+		aws ecs describe-task-definition --task-definition "$1"
+	else
+		aws --output json ecs list-task-definitions | jq -r '.taskDefinitionArns[] | sub("^[^/]+/"; "")'
+	fi
 }
-export -f ecs-services
+export -f task-def
+
+ecs-service() {
+	local help_text="Usage: ${FUNCNAME[0]} [OPTIONAL_ARGS] [options]
+	If no cluster and service name is provided, a list of services are returned.
+
+	Optional Arguments:
+	cluster		ECS Cluster the service is deployed on
+	service		Family name of the service
+
+	Options:
+	--help       Display this help message"
+
+	# Check if the '--help' flag is present
+	if [[ "$*" == *"--help"* ]]; then
+		echo "$help_text"
+		return 0 # Exit the function after printing help
+	fi
+
+	if [ "$#" -ne 2 ]; then
+		{
+			echo "CLUSTER SERVICE"
+			ecs-clusters | xargs -P4 -n1 -I {} aws --output json ecs list-services --cluster {} |
+				jq -r '.serviceArns[] | sub("^[^/]+/"; "") | sub("/"; " ")'
+		} | column -t
+	else
+		aws ecs describe-services --cluster "$1" --service "$2"
+	fi
+}
+export -f ecs-service
 
 ecs-tasks() {
 	# returns taskId only
