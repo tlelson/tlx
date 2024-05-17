26678c64316bb5cf137ab8b84f6e405c9727297a
Tim <tpj800@gmail.com>
2024-05-17 17:05:50 +1000

    Misc Tools: Route53 Profile and connectivity test tools

--- tools/misc.sh
+++ tools/misc.sh
@@ -121,3 +121,72 @@ nacls() {
 	fi
 }
 export -f nacls
+
+alias profiles='aws route53profiles list-profiles'
+alias profile-associations='aws route53profiles list-profile-associations'
+
+profile-resource-associations() {
+	if [ -z "$1" ]; then
+		echo "Must provide a profile ID as an argument"
+		return 1
+	fi
+	aws route53profiles list-profile-resource-associations --profile-id "$1"
+}
+
+alias connectivity-tests="aws --output json ec2 describe-network-insights-paths | jq -rc '.NetworkInsightsPaths[] | {
+	Name: (.Tags | map(select(.Key == \"Name\")) | .[0].Value),
+	Id: .NetworkInsightsPathId,
+}'"
+
+connectivity-test() {
+	if [ -z "$1" ]; then
+		echo "Must provide a connectivity test ID as an argument"
+		return 1
+	fi
+	aws ec2 describe-network-insights-paths \
+		--network-insights-path-ids "$1"
+}
+
+connectivity-test-runs() {
+	if [ -z "$1" ]; then
+		echo "Must provide a connectivity test ID as an argument"
+		return 1
+	fi
+
+	aws --output json ec2 describe-network-insights-analyses --network-insights-path-id "$1" | tee /tmp/nia.json | jq -c '.NetworkInsightsAnalyses |
+		sort_by(.StartDate)| .[] |  {
+		StartDate,
+		AnalysisId: .NetworkInsightsAnalysisId,
+		Status,
+		NetworkPathFound,
+	}'
+}
+
+connectivity-test-run() {
+	local help_text="Usage: ${FUNCNAME[0]} [Arguments]
+	Details of a specific analysis.
+
+	Use 'connectivity-tests' to find the test you want
+
+	Arguments
+	testID		Use 'connectivity-tests'
+	testRunID	Use 'connectivity-test-runs' to find latest.
+
+	Options:
+	--help       Display this help message"
+
+	# Check if the '--help' flag is present
+	if [[ "$*" == *"--help"* ]]; then
+		echo "$help_text"
+		return 0 # Exit the function after printing help
+	fi
+
+	if [ "$#" -ne 2 ]; then
+		echo "$help_text"
+		return 1
+	fi
+
+	aws ec2 describe-network-insights-analyses \
+		--network-insights-path-id "$1" \
+		--network-insights-analysis-ids "$2"
+}
