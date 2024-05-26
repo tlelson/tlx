9a34415a77d48386c285c9139b508d4a38520287
Tim <tpj800@gmail.com>
2024-05-26 14:44:17 +1000

    Formatting and fixes
    yea woops. got a bunch of stuff mixed in

--- tools/code-pipeline.sh
+++ tools/code-pipeline.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 cp-list() {
-	aws --output json codepipeline list-pipelines | jq -r '.pipelines[].name'
+    aws --output json codepipeline list-pipelines | jq -r '.pipelines[].name'
 }
 export -f cp-list
 cp-start() {
-	aws codepipeline start-pipeline-execution --name "$1"
+    aws codepipeline start-pipeline-execution --name "$1"
 }
 export -f cp-start
 
@@ -13,7 +13,7 @@ export -f cp-start
 #   - Allow no arguments for all pipelines and their status (change to cp-status)
 #   - Show Source commits of the execution at each stage
 cp-state() {
-	local help_text="Usage: ${FUNCNAME[0]} [options] [positional Args] [Optional Args]
+    local help_text="Usage: ${FUNCNAME[0]} [options] [positional Args] [Optional Args]
     Summarised current state of the specified pipeline.
 
     Options:
@@ -28,83 +28,84 @@ cp-state() {
     stage_name      StageName to restrict output to. e.g 'Dev'
     "
 
-	# TODO: For each executionId (at each stage, get the Source version of each)
-	# This is probably getting too complex for jq now.
+    # TODO: For each executionId (at each stage, get the Source version of each)
+    # This is probably getting too complex for jq now.
 
-	local full=0
-	local guess_name=0
-	local pipeline_name=""
-	local stage_name=""
+    local full=0
+    local guess_name=0
+    local pipeline_name=""
+    local stage_name=""
 
-	# Check if no arguments are provided
-	if [[ $# -eq 0 ]]; then
-		echo "$help_text"
-		return 1
-	fi
+    # Check if no arguments are provided
+    if [[ $# -eq 0 ]]; then
+        echo "$help_text"
+        return 1
+    fi
 
-	# Parse command line arguments
-	while [[ $# -gt 0 ]]; do
-		case "$1" in
-		-f | --full)
-			full=1
-			shift
-			;;
-		-g | --guess)
-			guess_name=1
-			shift
-			;;
-		--help)
-			echo "$help_text"
-			return 0
-			;;
-		-*)
-			echo "Unknown option: $1"
-			echo "$help_text"
-			return 1
-			;;
-		*)
-			if [[ -z "$pipeline_name" ]]; then
-				pipeline_name="$1"
-			elif [[ -z "$stage_name" ]]; then
-				stage_name="$1"
-			else
-				echo "Unexpected argument: $1"
-				echo "$help_text"
-				return 1
-			fi
-			shift
-			;;
-		esac
-	done
+    # Parse command line arguments
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+        -f | --full)
+            full=1
+            shift
+            ;;
+        -g | --guess)
+            guess_name=1
+            shift
+            ;;
+        --help)
+            echo "$help_text"
+            return 0
+            ;;
+        -*)
+            echo "Unknown option: $1"
+            echo "$help_text"
+            return 1
+            ;;
+        *)
+            if [[ -z "$pipeline_name" ]]; then
+                pipeline_name="$1"
+            elif [[ -z "$stage_name" ]]; then
+                stage_name="$1"
+            else
+                echo "Unexpected argument: $1"
+                echo "$help_text"
+                return 1
+            fi
+            shift
+            ;;
+        esac
+    done
 
-	if ((guess_name == 1)); then
-		names=$(cp-list | grep "$pipeline_name")
-		count=$(wc -l <<<"$names")
+    if ((guess_name == 1)); then
+        names=$(cp-list | grep "$pipeline_name")
+        count=$(wc -l <<<"$names")
 
-		if ((count == 0)); then
-			echo "could not find a pipeline containing pattern: $1"
-			return 1
-		fi
+        if ((count == 0)); then
+            echo "could not find a pipeline containing pattern: $1"
+            return 1
+        fi
 
-		if ((count > 1)); then
-			echo "Ambiguous pipeline glob pattern: $1"
-			echo "Got $count matching pipelines: "
-			while IFS= read -r line; do
-				echo -e "\t$line"
-			done <<<"$names"
-			return 1
-		fi
+        if ((count > 1)); then
+            echo "Ambiguous pipeline glob pattern: $1"
+            echo "Got $count matching pipelines: "
+            while IFS= read -r line; do
+                echo -e "\t$line"
+            done <<<"$names"
+            return 1
+        fi
 
-		pipeline_name="$names"
-	fi
+        pipeline_name="$names"
+    fi
 
-	state=$(aws --output json codepipeline get-pipeline-state --name "$pipeline_name")
+    state=$(aws --output json codepipeline get-pipeline-state --name "$pipeline_name")
 
-	if [ -n "$full" ]; then
-		echo "${state}" | jq
-	fi
+    if ((full == 1)); then
+        echo "${state}" | jq
+        return 0
+    fi
 
-	result=$(echo "${state}" | jq '
+    result=$(echo "${state}" | jq '
         {pipelineName, updated, stages: [
             .stageStates[] | select(.latestExecution.status) |
             if ( .stageName == "Source") and (.latestExecution.status == "Succeeded")  then
@@ -154,67 +155,67 @@ cp-state() {
             ]
         }
         ')
-	# --arg stage_name "$stage_name"
-	if [ -z "$stage_name" ]; then
-		echo "$result" | jq
-	else
-		echo "$result" | jq "{pipelineName, updated,
+    # --arg stage_name "$stage_name"
+    if [ -z "$stage_name" ]; then
+        echo "$result" | jq
+    else
+        echo "$result" | jq "{pipelineName, updated,
             stage: .stages[]| select(.stageName==\"$stage_name\")
         }"
-	fi
+    fi
 }
 export -f cp-state
 
 cp-approve() {
-	if [ -z "$1" ]; then
-		echo "provide a pipeline name as the first argument, and optionally, the stage as a second argument.  If only one stage is approvable, it will be approved."
-		echo "e.g ${FUNCNAME[0]} 'meta' "
-		echo "e.g ${FUNCNAME[0]} 'meta' 'Approval_To_Staging' "
-		return 1
-	fi
-	pipeline_name="$1"
-	# TODO: Could do this more efficiently with aws cmd
-	pipeline=$(cp-check "${pipeline_name}")
+    if [ -z "$1" ]; then
+        echo "provide a pipeline name as the first argument, and optionally, the stage as a second argument.  If only one stage is approvable, it will be approved."
+        echo "e.g ${FUNCNAME[0]} 'meta' "
+        echo "e.g ${FUNCNAME[0]} 'meta' 'Approval_To_Staging' "
+        return 1
+    fi
+    pipeline_name="$1"
+    # TODO: Could do this more efficiently with aws cmd
+    pipeline=$(cp-check "${pipeline_name}")
 
-	stage_to_approve=""
+    stage_to_approve=""
 
-	if [ -z "$2" ]; then
+    if [ -z "$2" ]; then
 
-		readarray approvable_stages < <(echo "${pipeline}" | jq -r '[.stages[] | select(.stageName|test("Approval.*")) | select(.status == "InProgress")  | .stageName] | .[]')
+        readarray approvable_stages < <(echo "${pipeline}" | jq -r '[.stages[] | select(.stageName|test("Approval.*")) | select(.status == "InProgress")  | .stageName] | .[]')
 
-		if [ ${#approvable_stages[@]} -ne 1 ]; then
-			echo "Approvable stages are: "
-			for stage in "${approvable_stages[@]}"; do
-				stage=$(echo "$stage" | tr -d '\n')
-				echo "\"${stage}\""
-			done
-			return 1
-		else
-			stage_to_approve=$(echo "${approvable_stages[0]}" | tr -d '\n')
-		fi
-	else
-		stage_to_approve="$2"
-	fi
+        if [ ${#approvable_stages[@]} -ne 1 ]; then
+            echo "Approvable stages are: "
+            for stage in "${approvable_stages[@]}"; do
+                stage=$(echo "$stage" | tr -d '\n')
+                echo "\"${stage}\""
+            done
+            return 1
+        else
+            stage_to_approve=$(echo "${approvable_stages[0]}" | tr -d '\n')
+        fi
+    else
+        stage_to_approve="$2"
+    fi
 
-	echo "approving stage: ${stage_to_approve}"
+    echo "approving stage: ${stage_to_approve}"
 
-	action=$(echo "${pipeline}" | jq -rc --arg stage "${stage_to_approve}" '.stages[] | select(.stageName == $stage) | .actions[] | select(.status == "InProgress") | {actionName, token}')
-	action_name=$(echo "$action" | jq -r '.actionName')
-	token=$(echo "$action" | jq -r '.token')
+    action=$(echo "${pipeline}" | jq -rc --arg stage "${stage_to_approve}" '.stages[] | select(.stageName == $stage) | .actions[] | select(.status == "InProgress") | {actionName, token}')
+    action_name=$(echo "$action" | jq -r '.actionName')
+    token=$(echo "$action" | jq -r '.token')
 
-	aws --output json codepipeline put-approval-result \
-		--pipeline-name "${pipeline_name}" \
-		--stage-name "${stage_to_approve}" \
-		--action-name "${action_name}" \
-		--result 'summary="",status=Approved' \
-		--token "${token}"
+    aws --output json codepipeline put-approval-result \
+        --pipeline-name "${pipeline_name}" \
+        --stage-name "${stage_to_approve}" \
+        --action-name "${action_name}" \
+        --result 'summary="",status=Approved' \
+        --token "${token}"
 
 }
 export -f cp-approve
 
 cp-status() {
-	# Define the help text
-	local help_text="Usage: ${FUNCNAME[0]} [OPTIONAL_ARGS] [options]
+    # Define the help text
+    local help_text="Usage: ${FUNCNAME[0]} [OPTIONAL_ARGS] [options]
 
     Optional Arguments:
     pipeline_name_filter    grep regex to filter pipelines
@@ -222,34 +223,34 @@ cp-status() {
     Options:
     --help                  Display this help message"
 
-	# Check if the '--help' flag is present
-	if [[ "$*" == *"--help"* ]]; then
-		echo "$help_text"
-		return 0
-	fi
+    # Check if the '--help' flag is present
+    if [[ "$*" == *"--help"* ]]; then
+        echo "$help_text"
+        return 0
+    fi
 
-	filter_pattern='"*"'
+    filter_pattern='"*"'
 
-	if [ "$#" -ne 0 ]; then
-		filter_pattern="$*"
-	fi
+    if [ "$#" -ne 0 ]; then
+        filter_pattern="$*"
+    fi
 
-	{
-		echo "PIPELINE STATUS LASTRUN"
-		aws --output json codepipeline list-pipelines | jq '.pipelines[].name' |
-			grep "${filter_pattern}" | xargs -P20 -n1 -I {} sh -c 'aws --output json \
+    {
+        echo "PIPELINE STATUS LASTRUN"
+        aws --output json codepipeline list-pipelines | jq '.pipelines[].name' |
+            grep "${filter_pattern}" | xargs -P20 -n1 -I {} sh -c 'aws --output json \
                     codepipeline list-pipeline-executions \
                     --pipeline-name "$1" --max-items 1 | jq -r --arg p "$1" \
                     '\''.pipelineExecutionSummaries[0] | [$p, .status, .startTime] |
                     .[2] |= sub(":[0-9]{2}\\.[0-9]{6}"; "")  | @tsv'\'' ' _ {}
 
-	} | column -t
+    } | column -t
 
 }
 export -f cp-status
 
 cp-definition() {
-	local help_text="Usage: ${FUNCNAME[0]} [Arguments] [Optional Arguments] [options]
+    local help_text="Usage: ${FUNCNAME[0]} [Arguments] [Optional Arguments] [options]
 
     Arguments:
     pipeline_name
@@ -260,29 +261,29 @@ cp-definition() {
     Options:
     --help          Display this help message"
 
-	# Check if the '--help' flag is present
-	if [[ "$*" == *"--help"* ]]; then
-		echo "$help_text"
-		return 1
-	fi
+    # Check if the '--help' flag is present
+    if [[ "$*" == *"--help"* ]]; then
+        echo "$help_text"
+        return 1
+    fi
 
-	if [ -z "$1" ]; then
-		echo "$help_text"
-		return 1
-	fi
-	cmd="aws --output json codepipeline get-pipeline --name $1"
+    if [ -z "$1" ]; then
+        echo "$help_text"
+        return 1
+    fi
+    cmd="aws --output json codepipeline get-pipeline --name $1"
 
-	if [ -n "$2" ]; then
-		cmd="$cmd --pipeline-version $2"
-	fi
+    if [ -n "$2" ]; then
+        cmd="$cmd --pipeline-version $2"
+    fi
 
-	eval "$cmd" | jq '{pipeline}'
+    eval "$cmd" | jq '{pipeline}'
 
 }
 export -f cp-definition
 
 cp-update() {
-	local help_text="Usage: ${FUNCNAME[0]} [ARGS] [options]
+    local help_text="Usage: ${FUNCNAME[0]} [ARGS] [options]
 
     Arguments:
     file-path   Path to a yaml file that describes the pipeline to be updated. e.g /tmp/pipeline.yaml
@@ -290,17 +291,17 @@ cp-update() {
     Options:
     --help       Display this help message"
 
-	# Check if the '--help' flag is present
-	if [[ "$*" == *"--help"* ]]; then
-		echo "$help_text"
-		return 1
-	fi
+    # Check if the '--help' flag is present
+    if [[ "$*" == *"--help"* ]]; then
+        echo "$help_text"
+        return 1
+    fi
 
-	if [ -z "$1" ]; then
-		echo "$help_text"
-		return 1
-	fi
+    if [ -z "$1" ]; then
+        echo "$help_text"
+        return 1
+    fi
 
-	aws codepipeline update-pipeline --cli-input-yaml "file://$1" >/dev/null
+    aws codepipeline update-pipeline --cli-input-yaml "file://$1" >/dev/null
 }
 export -f cp-update
