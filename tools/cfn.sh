9ae4d1cb5d06f4d578460ef6ec3b451c3e1e2f56
Tim <tpj800@gmail.com>
2024-05-27 14:18:57 +1000

    Fix indentation on cfn.sh

--- tools/cfn.sh
+++ tools/cfn.sh
@@ -6,146 +6,145 @@
 # specific and take an argument 'stack_name'.
 
 cfn-exports() {
-	# This returns all cfn exports.  It takes an optional argument that filters
-	# by glob pattern
-	# E.g:
-	#   cfn-exports | jtbl -n
-	#   cfn-exports 'subnet'
+    # This returns all cfn exports.  It takes an optional argument that filters
+    # by glob pattern
+    # E.g:
+    #   cfn-exports | jtbl -n
+    #   cfn-exports 'subnet'
 
-	exports=$(aws --output json cloudformation list-exports)
+    exports=$(aws --output json cloudformation list-exports)
 
-	jq_exp='.Exports[]'
+    jq_exp='.Exports[]'
 
-	if [ "$#" -ne 0 ]; then
-		jq_exp+="| select(.Name|test(\".*${*}.*\"))"
-	fi
+    if [ "$#" -ne 0 ]; then
+        jq_exp+="| select(.Name|test(\".*${*}.*\"))"
+    fi
 
-	# Tabulate response
-	jq_exp+='| {Stack: .ExportingStackId | sub("^[^/]+/"; "") | sub("/.*$"; ""), Name, Value}'
-	echo "${exports}" | jq -c "$jq_exp"
+    # Tabulate response
+    jq_exp+='| {Stack: .ExportingStackId | sub("^[^/]+/"; "") | sub("/.*$"; ""), Name, Value}'
+    echo "${exports}" | jq -c "$jq_exp"
 }
 export -f cfn-exports
 
 # TODO: Speedup
 cfn-resources() {
-	# This returns all resources deployed by all cfn stacks.  It takes an optional argument
-	# that defines the level of concurrency.
-	# E.g:
-	#   cfn-resources           # Uses 32 concurrent processes
-	#   cfn-resources 4         # Uses  4 concurrent processes
+    # This returns all resources deployed by all cfn stacks.  It takes an optional argument
+    # that defines the level of concurrency.
+    # E.g:
+    #   cfn-resources           # Uses 32 concurrent processes
+    #   cfn-resources 4         # Uses  4 concurrent processes
 
-	concurrency=32
+    concurrency=32
 
-	if [ -n "$2" ]; then
-		concurrency="$2"
-	fi
+    if [ -n "$2" ]; then
+        concurrency="$2"
+    fi
 
-	## Parrallel
+    ## Parrallel
 
-	task() {
-		aws --output json cloudformation list-stack-resources --stack-name "$1" |
-			jq -c --arg stack_name "$1" '.StackResourceSummaries[] | {StackName: $stack_name, ResourceType, PhysicalResourceId, LogicalResourceId}'
-	}
+    task() {
+        aws --output json cloudformation list-stack-resources --stack-name "$1" |
+            jq -c --arg stack_name "$1" '.StackResourceSummaries[] | {StackName: $stack_name, ResourceType, PhysicalResourceId, LogicalResourceId}'
+    }
 
-	stacks=$(aws --output json cloudformation list-stacks | jq -r '.StackSummaries[] | select(.StackStatus != "DELETE_COMPLETE") | .StackName')
+    stacks=$(aws --output json cloudformation list-stacks | jq -r '.StackSummaries[] | select(.StackStatus != "DELETE_COMPLETE") | .StackName')
 
-	(
-		for stack_name in ${stacks}; do
-			((i = i % concurrency)) # Exits with i. Can't exit on first error
-			((i++ == 0)) && wait
-			#echo "${stack_name}" &
-			task "${stack_name}" &
-		done
-	)
+    (
+        for stack_name in ${stacks}; do
+            ((i = i % concurrency)) # Exits with i. Can't exit on first error
+            ((i++ == 0)) && wait
+            #echo "${stack_name}" &
+            task "${stack_name}" &
+        done
+    )
 
 }
 export -f cfn-resources
 
 stack-events() {
-	event_limit=10
+    event_limit=10
 
-	if [ -z "$1" ]; then
-		echo "provide a stack name as the first argument"
-		return 1
-	fi
+    if [ -z "$1" ]; then
+        echo "provide a stack name as the first argument"
+        return 1
+    fi
 
-	if [ -n "$2" ]; then
-		event_limit="$2"
-	fi
+    if [ -n "$2" ]; then
+        event_limit="$2"
+    fi
 
-	aws --output json cloudformation describe-stack-events --stack-name "$1" \
-		--max-items "${event_limit}" | jq '.StackEvents | map({
+    aws --output json cloudformation describe-stack-events --stack-name "$1" \
+        --max-items "${event_limit}" | jq '.StackEvents | map({
         ResourceType, LogicalResourceId, ResourceStatus, Timestamp, ResourceStatusReason })'
 
 }
 export -f stack-events
 
 stack-failed() {
-	if [ -z "$1" ]; then
-		echo "provide a stack name as the first argument"
-		return 1
-	fi
-	stack_name="$1"
+    if [ -z "$1" ]; then
+        echo "provide a stack name as the first argument"
+        return 1
+    fi
+    stack_name="$1"
 
-	stack-events "$stack_name" 50 | jq '[
+    stack-events "$stack_name" 50 | jq '[
         .[] | select(.ResourceStatus|test(".*FAILED")) ]'
 }
 export -f stack-failed
 
 stack-params() {
 
-	if [ -z "$1" ]; then
-		echo "Output is JSON by default because the output may be used as input for deploy."
-		echo "${FUNCNAME[0]} \$stack_name | jtbl"
-		return 1
-	fi
-	stack_name="$1"
+    if [ -z "$1" ]; then
+        echo "Output is JSON by default because the output may be used as input for deploy."
+        echo "${FUNCNAME[0]} \$stack_name | jtbl"
+        return 1
+    fi
+    stack_name="$1"
 
-	aws --output json cloudformation describe-stacks --stack-name "$stack_name" |
-		jq -r '.Stacks[0].Parameters'
+    aws --output json cloudformation describe-stacks --stack-name "$stack_name" |
+        jq -r '.Stacks[0].Parameters'
 
 }
 export -f stack-params
 
 stack-status() {
 
-	stack_status=$(aws --output json cloudformation list-stacks |
-		jq -rc '.StackSummaries[] | [ .StackName[:60], .StackStatus,
+    stack_status=$(aws --output json cloudformation list-stacks |
+        jq -rc '.StackSummaries[] | [ .StackName[:60], .StackStatus,
             (.LastUpdatedTime // .CreationTime)
             ] | .[2] |= sub(":[0-9]{2}\\.[0-9]{6}"; "") | @tsv ' |
-		grep -v 'DELETE_COMPLETE')
+        grep -v 'DELETE_COMPLETE')
 
-	{
-		echo "STACKNAME STATUS UPDATED"
-		if [ "$#" -ne 0 ]; then
-			echo "$stack_status" | grep --color=auto "$*"
-		else
-			echo "$stack_status"
-		fi
-	} | column -t
+    {
+        echo "STACKNAME STATUS UPDATED"
+        if [ "$#" -ne 0 ]; then
+            echo "$stack_status" | grep --color=auto "$*"
+        else
+            echo "$stack_status"
+        fi
+    } | column -t
 
 }
 export -f stack-status
 
 stack-template() {
-	if [ -z "$1" ]; then
-		echo "Returns the template used to deploy a stack"
-		echo "${FUNCNAME[0]} \$stack_name"
-		return 1
-	fi
-	stack_name="$1"
+    if [ -z "$1" ]; then
+        echo "Returns the template used to deploy a stack"
+        echo "${FUNCNAME[0]} \$stack_name"
+        return 1
+    fi
+    stack_name="$1"
 
-	aws --output json cloudformation get-template --stack-name "$stack_name" |
-		jq -r '.TemplateBody'
+    aws --output json cloudformation get-template --stack-name "$stack_name" |
+        jq -r '.TemplateBody'
 
 }
 export -f stack-template
 
-alias stack-delete='aws cloudformation delete-stack --stack-name '
 alias stack-resources='aws cloudformation list-stack-resources --stack-name '
 
 stack-deploy() {
-	local help_text="Usage: ${FUNCNAME[0]} [Arguments] [OPTIONAL_ARGS] [options]
+    local help_text="Usage: ${FUNCNAME[0]} [Arguments] [OPTIONAL_ARGS] [options]
 
     Arguments:
     stack_name
@@ -157,57 +156,56 @@ stack-deploy() {
     Options:
     --help       Display this help message"
 
-	# Initialize variables
-	local stack_name=""
-	local template_file=""
-	local parameters_file=""
+    # Initialize variables
+    local stack_name=""
+    local template_file=""
+    local parameters_file=""
 
-	# Parse arguments
-	while [[ $# -gt 0 ]]; do
-		case "$1" in
-		--help)
-			echo "$help_text"
-			return 0
-			;;
-		*)
-			if [[ -z "$stack_name" ]]; then
-				stack_name="$1"
-			elif [[ -z "$template_file" ]]; then
-				template_file="$1"
-			elif [[ -z "$parameters_file" ]]; then
-				parameters_file="$1"
-			else
-				echo "Error: Unexpected argument '$1'"
-				echo "$help_text"
-				return 1
-			fi
-			shift
-			;;
-		esac
-	done
+    # Parse arguments
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+        --help)
+            echo "$help_text"
+            return 0
+            ;;
+        *)
+            if [[ -z "$stack_name" ]]; then
+                stack_name="$1"
+            elif [[ -z "$template_file" ]]; then
+                template_file="$1"
+            elif [[ -z "$parameters_file" ]]; then
+                parameters_file="$1"
+            else
+                echo "Error: Unexpected argument '$1'"
+                echo "$help_text"
+                return 1
+            fi
+            shift
+            ;;
+        esac
+    done
 
-	# Display help if requested or if required arguments are missing
-	if [[ -z "$stack_name" || -z "$template_file" ]]; then
-		echo "$help_text"
-		return 1
-	fi
+    # Display help if requested or if required arguments are missing
+    if [[ -z "$stack_name" || -z "$template_file" ]]; then
+        echo "$help_text"
+        return 1
+    fi
 
-	if [[ -n "$parameters_file" ]]; then
-		aws cloudformation deploy \
-			--capabilities CAPABILITY_NAMED_IAM \
-			--parameter-overrides "file://${parameters_file}" \
-			--stack-name "${stack_name}" --template "${template_file}"
-	else
-		aws cloudformation deploy \
-			--capabilities CAPABILITY_NAMED_IAM \
-			--stack-name "${stack_name}" --template "${template_file}"
-	fi
+    if [[ -n "$parameters_file" ]]; then
+        aws cloudformation deploy \
+            --capabilities CAPABILITY_NAMED_IAM \
+            --parameter-overrides "file://${parameters_file}" \
+            --stack-name "${stack_name}" --template "${template_file}"
+    else
+        aws cloudformation deploy \
+            --capabilities CAPABILITY_NAMED_IAM \
+            --stack-name "${stack_name}" --template "${template_file}"
+    fi
 
 }
 
 stack-delete() {
     local help_text="Usage: ${FUNCNAME[0]} [Arguments] [options]
-    This will force delete a stack. Use carefully.
 
     Arguments:
     stack_name
@@ -224,7 +222,6 @@ stack-delete() {
         echo "$help_text"
         return 1
     fi
-    stack_name="$1"
 
     # Parse command line arguments
     while [[ $# -gt 0 ]]; do
