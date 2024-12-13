96830956f6ac601739b6841644970d9525cf3991
Tim <tpj800@gmail.com>
2024-12-13 19:32:31 +1100

    Added checkhealth to test flake install
    I want to ensure that shell scripts are installed into - and run inside
    -the nix environment of the package.

--- flake.nix
+++ flake.nix
@@ -14,59 +14,79 @@
     }:
     utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+      };
       pythonPkgs = pkgs.python312Packages;
       #pyproject = pkgs.lib.importTOML ./pyproject.toml;
     in
     {
       # Executed by `nix build .`
-      packages.default = pythonPkgs.buildPythonPackage {
-        #pname = pyproject.project.name;
-        #inherit (pyproject.project) version;
-        pname = "tlx";
-        version = "0.1.1";
-        #format = "pyproject";
-        pyproject = true;
+      packages.default = pythonPkgs.buildPythonPackage
+        {
+          #pname = pyproject.project.name;
+          #inherit (pyproject.project) version;
+          pname = "tlx";
+          version = "0.1.1";
+          #format = "pyproject";
+          pyproject = true;
 
-        src = ./.;
+          src = ./.;
 
-        #build-system = [ pythonPkgs.hatchling ];
-        build-system = [ pythonPkgs.setuptools ];
+          #build-system = [ pythonPkgs.hatchling ];
+          build-system = [ pythonPkgs.setuptools ];
 
-        dependencies = with pythonPkgs; [
-          # Python dependencies
-          boto3
-          click
-        ];
+          dependencies = with pythonPkgs; [
+            # Python dependencies
+            boto3
+            click
+          ];
 
-        buidInputs = with pkgs; [
-          # Non Python dependencies
-          # Build and/or run-time dependencies that need to be compiled for the host machine
-          awscli2 # Although this is written in python it isn't a library. Its from nixpkgs
-        ];
-      };
+          nativeBuidInputs = with pkgs; [
+            makeWrapper # provides wrapProgram
+          ];
+
+          buidInputs = with pkgs; [
+            # Non Python dependencies
+            # Build and/or run-time dependencies that need to be compiled for the host machine
+            awscli2 # Although this is written in python it isn't a library. Its from nixpkgs
+          ];
+
+          #makeWrapperArgs = [ "stack" ];
+          postInstall = ''
+            wrapProgram $out/bin/checkhealth \
+                --set PATH ${pkgs.python312Packages.python.interpreter}/bin:$PATH \
+                --set SHELL ${pkgs.bash}/bin/bash
+          '';
+        };
 
       # Used by `nix develop`
       devShells.default = pkgs.mkShell
         {
           inputsFrom = [ self.packages.${system}.default ];
           packages = with pkgs; [
+            python312
             awscli2
             pyright
-            python312Packages.ipython
-            python312Packages.mypy
-            python312Packages.mypy-boto3-cloudformation
-            python312Packages.mypy-boto3-cloudformation
-            python312Packages.mypy-boto3-cloudformation
-            python312Packages.mypy-boto3-cloudformation
-            python312Packages.mypy-boto3-cloudformation
-          ];
-          shellHook = ''
-            # Needed because urllib appears twice on the Python path and awscli needs an
-            # old version
-            # https://github.com/NixOS/nixpkgs/issues/267864#issuecomment-2289482964
-            export PYTHONPATH=""
-          '';
+          ] ++ (with pkgs.python312Packages; [
+            setuptools
+            ipython
+            mypy
+            mypy-boto3-cloudformation
+            mypy-boto3-codepipeline
+            mypy-boto3-iam
+            mypy-boto3-logs
+            mypy-boto3-organizations
+            mypy-boto3-s3
+            mypy-boto3-secretsmanager
+          ]);
+          #shellHook = ''
+          ## NOTE: Breaks import of above modules
+          ## Needed because urllib appears twice on the Python path and awscli needs an
+          ## old version
+          ## https://github.com/NixOS/nixpkgs/issues/267864#issuecomment-2289482964
+          #export PYTHONPATH=""
+          #'';
         };
     });
 }
--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
     },
     scripts=[
         # This is the only tool that is used outside of an aws account context.
+        "tools/checkhealth",
         "tools/aws-list-accounts",
         "tools/stack",
         "tools/cfn.sh",
